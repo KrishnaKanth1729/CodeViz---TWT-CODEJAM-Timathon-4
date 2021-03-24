f3e598c03b9f9790955fd48e98f6b51dba87cd49
KrishnaKanth1729 <77482438+KrishnaKanth1729@users.noreply.github.com>
2021-03-24 13:38:13 +0530

    he

--- flowcode/tests.py
+++ flowcode/tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 
 def isValidSubsequence(array, sequence):
-    arrIdx = 0
-    seqIdx = 0
-    while arrIdx < len(array) and seqIdx < len(sequence):
-        if array[arrIdx] == sequence[seqIdx]:
-            seqIdx += 1
-        arrIdx += 1
-    return seqIdx == len(sequence)
+	arrIdx = 0
+	seqIdx = 0
+	while arrIdx < len(array) and seqIdx < len(sequence):
+		if array[arrIdx] == sequence[seqIdx]:
+			seqIdx += 1
+		arrIdx += 1
+	return seqIdx == len(sequence)
 
-k = isValidSubsequence([1, 2, 5, 6, -1], [2, 6, 1])
+k = isValidSubsequence([1, 2, 5, 6, -1], [2, 6, -1])
 print(k)
