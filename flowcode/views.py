5e4b138918ce54679f782180675e4086e6182622
KrishnaKanth1729 <77482438+KrishnaKanth1729@users.noreply.github.com>
2021-03-26 14:45:19 +0530

    reay

--- flowcode/views.py
+++ flowcode/views.py
@@ -18,17 +18,13 @@ from .forms import *
 
 
 def getdata(text):
-    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    chrome_options.binary_location = os.environ.get("CHROMEDRIVER_PATH")
 
-    chrome_options.binary_location = GOOGLE_CHROME_PATH
-
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
     driver.get('http://flowchart.js.org/')
     time.sleep(2)
