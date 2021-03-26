9b932d6fcbe1510a7237e124a6542da67794950f
KrishnaKanth1729 <77482438+KrishnaKanth1729@users.noreply.github.com>
2021-03-26 15:07:02 +0530

    pls

--- flowcode/views.py
+++ flowcode/views.py
@@ -19,14 +19,11 @@ from .forms import *
 
 def getdata(text):
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.headless = True
-    chrome_options.add_argument("window-size=1400,800")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get('http://flowchart.js.org/')
     time.sleep(2)
     elements = driver.find_elements_by_class_name('ace_text-input')
