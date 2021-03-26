1e41c50fd87f62aaa61490ae744e91f4155c7c15
KrishnaKanth1729 <77482438+KrishnaKanth1729@users.noreply.github.com>
2021-03-26 14:29:51 +0530

    reay

--- flowcode/views.py
+++ flowcode/views.py
@@ -18,14 +18,15 @@ from .forms import *
 
 
 def getdata(text):
+    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.headless = True
-    chrome_options.add_argument("window-size=1400,800")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.binary_location = GOOGLE_CHROME_PATH
+
+    driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
     driver.get('http://flowchart.js.org/')
     time.sleep(2)
